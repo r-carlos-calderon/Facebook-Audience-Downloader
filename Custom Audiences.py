2737bb3c031ed31b20d66711cd1ba5b8a7133634
r-carlos-calderon <rcarloscalderon@gmail.com>
2020-07-08 09:18:02 -0400

    base code downloads audience metadata

--- Custom Audiences.py
+++ Custom Audiences.py
@@ -6,7 +6,7 @@ import sys
 import os
 from pandas import json_normalize
 
-# THIS SCRIPT DOWNLOADS METADATA AS TWO FILES FOR THE FOLLOWING: AUDIENCES AND TARGETING CRITERIA OF ADSETS
+# THIS SCRIPT DOWNLOADS AUDIENCES METADATA AS CSV AND JSON
 
 # CONFIGS
 get_config = open('config.json')
@@ -41,39 +41,6 @@ r2 = json.loads(r1)
 r3 = json.dumps(r2['data'])
 r4 = json.loads(r3)
 df_acct = pd.DataFrame(r4)
-
-# BEGIN DOWNLOADING ALL ADSET TARGETING METADATA
-all_adsets_json = []
-total_num_adsets = 0
-for ind_a in df_acct.index:  # ITERATE THROUGH ALL AD ACCOUNTS FROM STEP 1 TO DISCOVER ALL ADSETS (STEP 2)
-    acct = str(df_acct['account_id'][ind_a])
-    res = requests.get(f'https://graph.facebook.com/v7.0/act_{acct}/adsets?fields=id,account_id&limit=9999&access_token={token}')
-    r1 = res.text
-    r2 = json.loads(r1)
-    r3 = json.dumps(r2['data'])
-    r4 = json.loads(r3)
-    df_adset = pd.DataFrame(r4)
-    ind_b = 0
-    for ind_b in df_adset.index:  # ITERATE THROUGH ALL ADSETS FROM STEP 2 >> DOWNLOAD ADSET TARGETING METADATA
-        adset_id = str(df_adset['id'][ind_b])
-        res = requests.get(f'https://graph.facebook.com/v7.0/{adset_id}?fields=id,status,targeting&access_token={token}')
-        r1 = res.text
-        r2 = json.loads(r1)
-        r2.update(add_date)
-        all_adsets_json.append(r2)
-    num_adsets = ind_b + 1
-    total_num_adsets += num_adsets
-    sys.stdout.write(f'Downloaded {num_adsets} adsets metadata from act_{acct}\n')  # OUTPUT FOR LOG FILE
-all_adsets_pretty_json = json.dumps(all_adsets_json, indent=2)
-print(all_adsets_pretty_json, file=open(f'{file_path}/adset_targeting_{audit_date}.json', 'w'))  # SAVE RAW ADSET TARGETING METADATA AS JSON
-open_adsets = open(f'{file_path}/adset_targeting_{audit_date}.json')
-load_adsets = json.load(open_adsets)
-open_adsets.close()
-final_adsets = json_normalize(load_adsets)
-final_adsets.to_csv(f'{file_path}/adset_targeting_trimmed_{audit_date}.csv', columns=['id', 'status', 'audit_date', 'targeting.age_max', 'targeting.age_min', 'targeting.custom_audiences', 'targeting.geo_locations.countries', 'targeting.geo_locations.location_types', 'targeting.publisher_platforms', 'targeting.facebook_positions', 'targeting.instagram_positions', 'targeting.device_platforms', 'targeting.flexible_spec', 'targeting.excluded_geo_locations.regions', 'targeting.geo_locations.cities', 'targeting.excluded_geo_locations.cities', 'targeting.geo_locations.regions', 'targeting.geo_locations.geo_markets', 'targeting.app_install_state', 'targeting.user_device', 'targeting.user_os', 'targeting.excluded_custom_audiences', 'targeting.excluded_geo_locations.geo_markets', 'targeting.excluded_product_audience_specs', 'targeting.product_audience_specs'], index=False, sep=',', encoding='utf-8')  # TRIM UNNECESSARY ADSET TARGETING METADATA AND SAVE AS CSV
-numacct = ind_a + 1
-sys.stdout.write(f'\nDownloaded total of {total_num_adsets} adsets across {numacct} accounts in business account #{business_account}.\n\n')
-# END DOWNLOADING ALL AD SET TARGETING METADATA
 
 # BEGIN DOWNLOADING ALL AUDIENCE METADATA
 all_audiences_json = []
