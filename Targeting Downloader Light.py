409cbb02984a95a0bf4f823253371bf39bbefbee
r-carlos-calderon <rcarloscalderon@gmail.com>
2020-07-15 15:26:21 -0400

    init

--- Targeting Downloader Light.py
+++ Targeting Downloader Light.py
@@ -8,7 +8,7 @@ import urllib.parse
 # UNIVERSAL VARS
 today = datetime.date.today()
 audit_date = str(today)
-folder = 'AUDIENCE_TARGETING'
+folder = 'ADSETS_TARGETING'
 path = f'{folder}/{audit_date}'
 if not os.path.exists(f'{path}'):
     os.makedirs(f'{path}')
@@ -21,12 +21,12 @@ set_config = json.load(get_config)
 get_config.close()
 token = set_config['token']
 business_account = set_config['business_account']
-cust_fields = set_config['cust_fields']
-cust_limit = set_config['cust_limit']
+adset_fields = set_config['adset_fields']
+adset_limit = set_config['adset_limit']
 FBGraphRequest = 'https://graph.facebook.com/v7.0'
 params = {
-    'fields': cust_fields,
-    'limit': cust_limit,
+    'fields': adset_fields,
+    'limit': adset_limit,
     'access_token': token
 }
 request_params = urllib.parse.urlencode(params, doseq=True)
@@ -41,21 +41,22 @@ r4 = json.loads(r3)
 df = pd.DataFrame(r4)
 
 # BEGIN DOWNLOADING ALL AUDIENCE METADATA
-business_audiences = []
-business_filename = f'{business_account}_audiences_{audit_date}'  # NAME OF FILE FOR ALL AUDIENCES IN BUSINESS ACCOUNT
+business_adsets = []
+business_filename = f'{business_account}_adsets_{audit_date}'  # NAME OF FILE FOR ALL AUDIENCES IN BUSINESS ACCOUNT
 for ind_a in df.index:  # ITERATE THROUGH ALL AD ACCOUNTS FROM STEP 1 TO DISCOVER ALL AUDIENCES (STEP 2)
     acct = str(df['id'][ind_a])
     res = requests.get(
-        f'{FBGraphRequest}/{acct}/customaudiences?{request_params}')
+        f'{FBGraphRequest}/{acct}/adsets?{request_params}')
     r1 = res.text
     r2 = json.loads(r1)
-    num_results = len(r2)
+    r3 = r2['data']
+    num_results = len(r3)
     # SKIP ACCOUNTS WITH NO CUSTOM AUDIENCES
     if num_results != 0:
         while True:  # GET NEXT PAGE RESULTS (GRAPH API ENDPOINT) REF: https://thd.co/2WiLrf2
             try:
                 for pg_results in r2['data']:
-                    business_audiences.append(pg_results)
+                    business_adsets.append(pg_results)
                 # ATTEMPT TO MAKE A REQUEST FOR THE NEXT PAGE OF DATA, IF IT EXISTS
                 get_next = r2['paging']['next']
                 r2 = requests.get(
@@ -65,5 +66,5 @@ for ind_a in df.index:  # ITERATE THROUGH ALL AD ACCOUNTS FROM STEP 1 TO DISCOVE
                 break
 
 # SAVE AUDIENCE METADATA FROM ALL AD ACCOUNTS AS JSON FILE
-business_audiences_pjson = json.dumps(business_audiences, indent=2)  # CONVERTS BOOLEAN VALUES TO TEXT STRINGS
-print(business_audiences_pjson, file=open(f'{file_path}/{business_filename}.json', 'w'))
+business_adsets_pjson = json.dumps(business_adsets, indent=2)  # CONVERTS BOOLEAN VALUES TO TEXT STRINGS
+print(business_adsets_pjson, file=open(f'{file_path}/{business_filename}.json', 'w'))
